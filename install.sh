a699cb47cbef8fee0aabd9d1e19d7c86f7f2f5c4
Mike Smiley <msmiley@users.noreply.github.com>
2017-09-07 10:01:23 -0400

    build node instead of downloading binary

--- install.sh
+++ install.sh
@@ -253,10 +253,14 @@ node(){
   
   echo :Installing Node $NODE_VERSION
   
-  DOWNLOAD https://nodejs.org/dist/"$NODE_VERSION/node-$NODE_VERSION-$1-$2.tar.gz" node.tar.gz
-  tar xzf node.tar.gz
-  mv "node-$NODE_VERSION-$1-$2" node
-  rm -f node.tar.gz
+  DOWNLOAD https://nodejs.org/dist/"$NODE_VERSION/node-$NODE_VERSION.tar.gz" node.tar.gz       
+  tar xzf node.tar.gz                                                                          
+  cd node-$NODE_VERSION                                                                   
+  ./configure --prefix=/root/.c9/node                                 
+  make                                                                                    
+  make install                                                                                                   
+  cd ..                                                                                   
+  rm -rf node.tar.gz node-$NODE_VERSION
 
   # use local npm cache
   "$NPM" config -g set cache  "$C9_DIR/tmp/.npm"
