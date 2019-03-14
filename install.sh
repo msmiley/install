03554c609b672bca082cbdaeda26261584a8aabc
Mike Smiley <msmiley@alumni.rice.edu>
2019-03-14 12:32:59 -0400

    disable collab 3

--- install.sh
+++ install.sh
@@ -389,15 +389,6 @@ tmux_install(){
 
 collab(){
   echo :Skipping Collab Dependencies
-  # "$NPM" cache verify
-  # "$NPM" install sqlite3@3.1.4
-  # "$NPM" install sequelize@2.0.0-beta.0
-  # mkdir -p "$C9_DIR"/lib
-  # cd "$C9_DIR"/lib
-  # DOWNLOAD https://raw.githubusercontent.com/c9/install/master/packages/sqlite3/linux/sqlite3.tar.gz sqlite3.tar.gz
-  # tar xzf sqlite3.tar.gz
-  # rm sqlite3.tar.gz
-  # ln -sf "$C9_DIR"/lib/sqlite3/sqlite3 "$C9_DIR"/bin/sqlite3
 }
 
 nak(){
